32fd58f0b7237240eec5fb93b250b4afdf9a5b36
bharti025 <20001003032bharti@dcrustm.org>
2025-10-04 14:09:26 +0530

    done

--- src/components/FormState.js
+++ src/components/FormState.js
@@ -17,10 +17,10 @@ const handleSubmit=(e)=>{
   return (
 
       <form id="info-form" onSubmit={handleSubmit}>
-        <input id="full_name" type="text" onChange={()=>setName(e.target.value)}/>
-           <input id="email" type="email"onChange={()=>setEmail(e.target.value)}/>
-           <input id="password" type="password" onChange={()=>setPassword(e.target.value)}/>
-           <input type="password" id="password_confirmation" onChange={()=>setPassword_confirmation(e.target.value)}/>
+        <input id="full_name" type="text" onChange={(e)=>setName(e.target.value)}/>
+           <input id="email" type="email" onChange={(e)=>setEmail(e.target.value)}/>
+           <input id="password" type="password" onChange={(e)=>setPassword(e.target.value)}/>
+           <input type="password" id="password_confirmation" onChange={(e)=>setPassword_confirmation(e.target.value)}/>
            <button type='submit'>Submit</button>
       </form>
   
